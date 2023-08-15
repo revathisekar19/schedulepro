d746df304f02d26ce7d3b7786f22d7c84998291c
Revathi Sekar <revathisekar2017@gmail.com>
2023-08-15 23:20:44 +0530

    Committing after performing get schedule and change assets

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,9 +1,44 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
+  private basegetUrl = '/internal/assetids';
 
-  constructor() { }
+  private username = 'bsmuser';
+  private password = 'test';
+  private secretToken = 'aRJSb9s2lr24jYnjQAVj';
+
+  constructor(private http: HttpClient) { }
+
+  getAssetId() {
+    const headers = new HttpHeaders({
+      'Authorization': 'Basic ' + btoa(this.username + ':' + this.password)
+    });
+    return this.http.get(this.basegetUrl, { headers });
+  }
+
+  scheduleMaintenance(maintenanceDetails: any) {
+    const baseUrl = `/asset/maintenance/${maintenanceDetails.assetId}/${maintenanceDetails.startDate}/${maintenanceDetails.stopDate}`;
+
+    const headers = new HttpHeaders({
+      'secretToken': this.secretToken,
+      'Authorization': 'Basic ' + btoa(this.username + ':' + this.password)
+    });
+
+    return this.http.post(baseUrl, {}, { headers });
+  }
+
+
+  changeAssetStatus(assetId: number, active: boolean) {
+    const apiUrl = `asset/set-asset-status/${assetId}`;
+    const params = { active: active.toString() };
+    const headers = new HttpHeaders({
+      'Authorization': 'Basic ' + btoa(this.username + ':' + this.password)
+    });
+
+    return this.http.post(`${apiUrl}?active=${active}`, {}, {params, headers });
+  }
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,72 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from './api.service';
+import { HttpClient } from '@angular/common/http';
+import {DatePipe} from "@angular/common";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'schedulepro';
+  //selectedAssetId: any
+  assetIds: any;
+  selectedAssetId: any;
+  startDate: any;
+  stopDate: any;
+  assetID: any;
+  currentDate = new Date();
+
+  constructor(private apiservice: ApiService, private http: HttpClient,private datePipe : DatePipe) {
+    this.scheduleMaintenance();
+  }
+
+  ngOnInit() {
+    this.getAssetIds();
+  }
+
+  getAssetIds() {
+    this.apiservice.getAssetId().subscribe(
+      (data: any) => {
+        this.assetIds = data;
+        console.log(this.assetIds);
+      },
+      (error) => {
+        console.error('Error fetching asset IDs:', error);
+      }
+    );
+  }
+
+  scheduleMaintenance() {
+    const maintenanceDetails = {
+      assetId: this.selectedAssetId,
+      startDate: this.datePipe.transform(this.startDate,'yyyy-MM-dd'),
+      stopDate: this.datePipe.transform(this.stopDate,'yyyy-MM-dd')
+    };
+
+    this.apiservice.scheduleMaintenance(maintenanceDetails)
+      .subscribe(
+        (res) => {
+          console.log('Maintenance scheduled successfully.',res);
+        },
+        (error) => {
+          console.error('Error scheduling maintenance:', error);
+        }
+      );
+  }
+
+  changeAssetStatus(active: boolean) {
+    const assetId = this.selectedAssetId;
+    this.apiservice.changeAssetStatus(assetId, active).subscribe(
+      (res) => {
+        alert('Asset status changed successfully');
+        console.log('Asset status changed successfully',res);
+      },
+      (error) => {
+        alert('Error changing asset status')
+        console.error('Error changing asset status', error);
+      }
+    );
+  }
 }
